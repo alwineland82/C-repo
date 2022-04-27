1d1a7011458501f1e655422f8dd7a0e78e733f22
al_wineland <alwineland@yandex.ru>
2022-04-27 20:49:04 +1100

    first homework ex

---
+++ prog01/Program.cs
@@ -0,0 +1,4 @@
+﻿float x = float.Parse(Console.ReadLine()), y = float.Parse(Console.ReadLine());
+string[] reply = new string[2];
+reply = new[] {"NO", "YES"};
+Console.WriteLine(reply[Convert.ToInt32(y == x / y)]); 
