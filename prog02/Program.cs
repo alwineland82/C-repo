665979e9c3c028c63b8d6db9e6b7e215cb522e81
al_wineland <alwineland@yandex.ru>
2022-04-27 21:43:02 +1100

    homework ex 2

---
+++ prog02/Program.cs
@@ -0,0 +1,10 @@
+﻿string Weekday(int num){
+    num--;
+    string[] days = new string[7];
+    days = new[] {"Monday", "Tuesday", "Wendnesday", 
+                  "Thursday", "Friday", "Saturday", "Sunday"};
+    return days[num];               
+} 
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Weekday(num));
