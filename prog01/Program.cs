d6525194e077770aa78835ebb36cec9fe12e5084
al_wineland <alwineland@yandex.ru>
2022-04-27 23:26:25 +1100

    prog03

--- prog01/Program.cs
+++ prog01/Program.cs
@@ -1,4 +1,3 @@
 ﻿float x = float.Parse(Console.ReadLine()), y = float.Parse(Console.ReadLine());
-string[] reply = new string[2];
-reply = new[] {"NO", "YES"};
+string[] reply = {"NO", "YES"};
 Console.WriteLine(reply[Convert.ToInt32(y == x / y)]); 
--- prog03/Program.cs
+++ prog03/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿void Even(int num)
+{
+    string[] reply = {"NO", "YES"};
+    Console.WriteLine(reply[Convert.ToInt32(num % 2 == 0)]);
+}
+int num = int.Parse(Console.ReadLine());
+Even(num);
