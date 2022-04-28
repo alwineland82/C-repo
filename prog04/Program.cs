bbd0150ab3436bc459da6d2a7c791dbff7826f94
al_wineland <alwineland@yandex.ru>
2022-04-28 13:01:56 +1100

    prog04

---
+++ prog04/Program.cs
@@ -0,0 +1,13 @@
+﻿void Even_Numbers(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i);
+            Console.Write(" ");
+        }
+    }
+}
+int x = int.Parse(Console.ReadLine());
+Even_Numbers(x);
