a0baa0a01f85f543d142384a048487660f904206
al_wineland <alwineland@yandex.ru>
2022-04-28 18:18:23 +1100

    prog08 & prog09

---
+++ prog08/Program.cs
@@ -0,0 +1,13 @@
+﻿string Is_Divisor(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        return $"{num1} is dividable by {num2}";
+    } else
+    {
+        return $"{num1} is not dividable by {num2}, the remainder is {num1 % num2}";
+    }
+}
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine(Is_Divisor(x, y));
---
+++ prog09/Program.cs
@@ -0,0 +1,12 @@
+﻿string Third_Number(string num)
+{
+    if (num.Length >= 3)
+    {
+        return Convert.ToString(num[2]); 
+    }else
+    {
+        return $"In number {num} there is no third digit";
+    }
+}
+string x = Console.ReadLine();
+Console.WriteLine(Third_Number(x));
