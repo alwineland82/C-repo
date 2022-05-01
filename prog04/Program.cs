0c7c24d2c8e0cc5e79a3b4ae4d73d15af8304b0a
al_wineland <alwineland@yandex.ru>
2022-05-01 20:36:11 +1100

    corrected

--- prog04/Program.cs
+++ prog04/Program.cs
@@ -1,12 +1,9 @@
 ﻿void Even_Numbers(int number)
 {
-    for (int i = 1; i <= number; i++)
+    for(int i = 2; i <= number; i += 2)
     {
-        if (i % 2 == 0)
-        {
-            Console.Write(i);
-            Console.Write(" ");
-        }
+        Console.Write(i);
+        Console.Write(" ");
     }
 }
 int x = int.Parse(Console.ReadLine());
