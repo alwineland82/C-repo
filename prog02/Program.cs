93e7a6f2fd931f340e60e33200c80efc5bd5804b
al_wineland <alwineland@yandex.ru>
2022-05-01 12:30:50 +1100

    corrected

--- prog02/Program.cs
+++ prog02/Program.cs
@@ -1,10 +1,43 @@
-﻿string Weekday(int num)
- {
-    num--;
-    string[] days = {"Monday", "Tuesday", "Wendnesday", 
-                  "Thursday", "Friday", "Saturday", "Sunday"};
-    return days[num];               
-} 
-
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine(Weekday(num));
+﻿string day = Console.ReadLine();
+switch (day)
+    {
+        case "1":
+            Console.WriteLine("Monday");
+        break;
+    }
+switch (day)
+    {
+        case "2":
+            Console.WriteLine("Tuesday");
+        break;
+    }
+switch (day)
+    {
+        case "3":
+            Console.WriteLine("Wednesday");
+        break;
+    }
+switch (day)
+    {
+        case "4":
+            Console.WriteLine("Thursday");
+        break;
+    }
+switch (day)
+    {
+        case "5":
+            Console.WriteLine("Friday");
+        break;
+    }
+switch (day)
+    {
+        case "6":
+            Console.WriteLine("Saturday");
+        break;
+    }
+switch (day)
+    {
+        case "7":
+            Console.WriteLine("Sunday");
+        break;
+    }
