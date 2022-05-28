7ad339624a73b14c81fa5f586562e046d9d28dcc
alwineland82 <103864391+alwineland82@users.noreply.github.com>
2022-05-29 10:01:52 +1100

    Update CopyCat.cs

--- CopyCat.cs
+++ CopyCat.cs
@@ -4,17 +4,17 @@ using System.Linq;
 class Program {
   static void Main() {
       float[] x = {1, 2, 3};
-      float[] y = CopyCat(x);
+      float[] y = CopyArray(x);
       x[0] = 100;
       foreach(var i in x) Console.WriteLine(i);
       foreach(var i in y) Console.WriteLine(i);
   }
-  static int[] CopyCat(int[] arg) => (from i in arg select i).ToArray();
-  static string[] CopyCat(string[] arg) => (from i in arg select i).ToArray();
-  static float[] CopyCat(float[] arg) => (from i in arg select i).ToArray();
-  static double[] CopyCat(double[] arg) => (from i in arg select i).ToArray();
-  static char[] CopyCat(char[] arg) => (from i in arg select i).ToArray();
-  static long[] CopyCat(long[] arg) => (from i in arg select i).ToArray();
-  static decimal[] CopyCat(decimal[] arg) => (from i in arg select i).ToArray();
+  static int[] CopyArray(int[] arg) => (from i in arg select i).ToArray();
+  static string[] CopyArray(string[] arg) => (from i in arg select i).ToArray();
+  static float[] CopyArray(float[] arg) => (from i in arg select i).ToArray();
+  static double[] CopyArray(double[] arg) => (from i in arg select i).ToArray();
+  static char[] CopyArray(char[] arg) => (from i in arg select i).ToArray();
+  static long[] CopyArray(long[] arg) => (from i in arg select i).ToArray();
+  static decimal[] CopyArray(decimal[] arg) => (from i in arg select i).ToArray();
   
 }
