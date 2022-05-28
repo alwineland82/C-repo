7eeb7c1d60e4ed8fd59bc31746badac51ad1ab26
alwineland82 <103864391+alwineland82@users.noreply.github.com>
2022-05-28 16:40:39 +1100

    Create ScalingZoom.cs

---
+++ ScalingZoom.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+//using System.Linq;
+
+
+class Program {
+  static void Main() {
+    //string arg = Console.ReadLine() ?? "";
+    Zoom( "(0,0) (2,0) (2,2) (0,2)", "2.8");
+  }
+ //_____________________________________________________________________________ 
+  static float[,] Zoom(string arg, string change)
+  {
+    Regex regex = new Regex(@"[0-9]+");
+    MatchCollection matches = regex.Matches(arg);
+    //----------------------------------------------------------
+    float buffer;
+    float[,] result = new float[4,2];
+    int counterRow = 0, counterColumn = 0;
+    string axis = "x";
+    Console.WriteLine($"New coordinates scaled by {change} = ");
+    //-----------------------------------------------------------
+    foreach (Match match in matches)
+    {
+        if(counterColumn == 1) axis = "y";
+        if(counterColumn > 1) counterColumn = 0;
+        if(counterColumn == 0) axis = "x";
+        buffer = int.Parse(match.Value) * float.Parse(change);
+        Console.Write($"{axis} = {buffer}" + "   ");
+        if(counterColumn == 1) Console.WriteLine();
+        result[counterRow, counterColumn] = buffer;
+        if(counterColumn == 1) {counterRow++;}
+        counterColumn++;
+        
+    }
+    //------------------------------------------------------------
+    Console.Beep();
+    Thread.Sleep(100);
+    Console.Beep();
+    Thread.Sleep(100);
+    Console.Beep();
+    return result;
+  }//___________________________________________________________________________
+}
