2963d21d60fd3398c0cb68fd3f5e8f79264bfe38
alwineland82 <103864391+alwineland82@users.noreply.github.com>
2022-06-02 12:37:09 +1100

    Update QuickSort.cs

--- QuickSort.cs
+++ QuickSort.cs
@@ -8,8 +8,8 @@ class Program
 {
     static void Main()
     {
-      int[] x = {2, 5, 7, 3, 6, 8, 3, 4, 7, 6, 8, 3};
-      x = QuickSort(x);
+      int[] array = {2, 5, 7, 3, 6, 8, 3, 4, 7, 6, 8, 3};
+      array = QuickSort(array);
       Display(x);
       
     } //*****************************************************************************
@@ -17,8 +17,8 @@ class Program
     {
       if(x.Length > 1)
       {
-        var bigger = (from i in x[0..^1] where i > x[^1] select i).ToArray();
-        var smaller = (from i in x[0..^1] where i < x[^1] select i).ToArray();
+        var bigger = (from i in x where i > x[^1] select i).ToArray();
+        var smaller = (from i in x where i < x[^1] select i).ToArray();
         var equal = (from i in x where i == x[^1] select i).ToArray();
         return Unify(QuickSort(bigger), equal, QuickSort(smaller));
       }else return x;
