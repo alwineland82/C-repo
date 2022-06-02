ec094979d81f763698708473324cca628dc316f8
alwineland82 <103864391+alwineland82@users.noreply.github.com>
2022-06-02 13:37:59 +1100

    Update LeastLine.cs

--- LeastLine.cs
+++ LeastLine.cs
@@ -6,35 +6,33 @@ using static System.Console;
 
 class Program {
   static void Main() {
-    Program program = new Program();
-    int[,] x = Generator(6, 4);
+    
+    int[,] x = Generator(7, 3);
     DisplayArray(x);
     WriteLine();
-    
-    program.MinorLine(x);
+    x = ArraySorter(x);
+    DisplayArray(x);
   }
-//___________________________________________________________________________________
-  void MinorLine(int[,] arr)
-  {
-      int counter = 0;
-      int line_number = 0;
-      int Min_Line = 0;
-      double min = double.PositiveInfinity;
-      int temp_min = 0;
-      foreach(var i in arr)
-      {
-          counter++;
-          temp_min += i;
-          if(counter == arr.GetLength(1)) 
-          {
-            if(temp_min < min) {min = temp_min; Min_Line = line_number;}
-            counter = 0; 
-            line_number++;
-            temp_min = 0;
-          }
-      }WriteLine($"Line {Min_Line + 1}");
-  }
-//______________________________________________________________________________________
+//*****************************************************************************
+ static int[,] ArraySorter(int[,] arr, int index = 0)
+ {
+     if(index == arr.GetLength(0))
+     {
+         return arr;
+     }
+     for(int i = 0; i < arr.GetLength(1); i++)
+     {
+         for(int j = 0; j < arr.GetLength(1) - 1; j++)
+         {
+             if(arr[index, j] < arr[index, j + 1])
+             {
+                 (arr[index, j], arr[index, j + 1]) = (arr[index, j + 1], arr[index, j]);
+             }
+         }
+     }index++;
+     return ArraySorter(arr, index);
+ }
+//********************************************************************************
   static int[,] Generator(int m, int n)
   {
       int[,] result = new int[m, n];
@@ -57,4 +55,5 @@ class Program {
           if(counter == arr.GetLength(1)) {WriteLine(); counter = 0;}
       }
   }
+  
 }
